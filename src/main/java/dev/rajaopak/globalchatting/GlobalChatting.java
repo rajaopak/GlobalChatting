3abe796b02bc6be0a57b0922adafc27e42750109
rajaopak <ahmadnafis0302@gmail.com>
2022-06-11 20:14:10 +0700

    Initial Commit

---
+++ src/main/java/dev/rajaopak/globalchatting/GlobalChatting.java
@@ -0,0 +1,16 @@
+package dev.rajaopak.globalchatting;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class GlobalChatting extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
